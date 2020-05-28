ee5c118e68519f7aab67ea7414805653b2c1019d
sridhar <sridharmay1@gmail.com>
2020-05-29 00:07:35 +0530

    Initial commit-"setup of spring boot and Mysql"

---
+++ AU-Management-system/src/test/java/com/accolite/AUManagementsystem/AuManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolite.AUManagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
